55b0c1fe70774d717eeb22084eb3b53d5fede41c
Gabriel Felipe <gabrielfelipe11102002@gmail.com>
2023-01-28 23:10:44 -0300

    AGORA DEU CERTO

--- JavaScript/Js.js
+++ JavaScript/Js.js
@@ -1,4 +1,20 @@
-
+function clickmenu() {
+    const itens = document.querySelectorAll('.itens')
+    const navegation = document.getElementById('inavegation')
+    
+    itens.forEach(item => {
+        if (item.style.display == 'block') {
+            item.style.display = 'none'
+            navegation.style.width = '40px'
+            //navegation.style.height = '40px'
+            
+        } else {
+            item.style.display = 'block'
+            navegation.style.width = '300px'
+            //navegation.style.height = '500px'   
+        }
+    })
+}
 
 
 
