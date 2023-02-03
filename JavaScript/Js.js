6165fe42f3eb450d8674d41d2a32817628376604
Gabriel Felipe <gabrielfelipe11102002@gmail.com>
2023-02-03 11:21:34 -0300

    att

--- JavaScript/Js.js
+++ JavaScript/Js.js
@@ -1,18 +1,14 @@
 function clickmenu() {
     const itens = document.querySelectorAll('.itens')
     const navegation = document.getElementById('inavegation')
-
-    
+ 
     itens.forEach(item => {
         if (item.style.display == 'block') {
             item.style.display = 'none'
-            navegation.style.width = '40px'
-            //navegation.style.height = '40px'
-            
+            navegation.style.width = '40px'  
         } else {
             item.style.display = 'block'
-            navegation.style.width = '300px'
-            //navegation.style.height = '500px'   
+            navegation.style.width = '300px'   
         }
     })
 }
